e6cf4f60b9b07556a19e6681bf2bfb899d912d9b
Peter <57006688+TheOfficialPeter@users.noreply.github.com>
2023-01-07 20:33:12 +0000

    Update main.js

--- main.js
+++ main.js
@@ -35,7 +35,13 @@ window.onload = function()
         document.getElementById('nav-drawer').style.backgroundColor = "#1f0506";
         document.getElementById('page-footer').remove();
         document.getElementById('section-0').remove();
+    }
+    catch
+    {
 
+    }
+    
+    try {
         var navItems = document.getElementsByClassName('toggle-submenu');
         var cardTitles = document.getElementsByClassName('photo-tile-text');
         var subNavList = document.getElementsByClassName('list-group-item list-group-item-action  show');
@@ -47,13 +53,21 @@ window.onload = function()
             cardTitles[i].childNodes[1].style.fontFamily = "monospace";
             cardTitles[i].style.height = "100%";
 
+            cardTitles[i].childNodes[1].style.height = "60px";
             cardTitles[i].parentNode.parentNode.parentNode.style.boxShadow = "none";
             cardTitles[i].parentNode.parentNode.parentNode.style.border = "none";
             cardTitles[i].parentNode.parentNode.parentNode.style.borderRadius = "5px";
             cardTitles[i].parentNode.parentNode.parentNode.style.background = "none";
-            cardTitles[i].parentNode.parentNode.parentNode.style.height = "75px";
+            cardTitles[i].parentNode.parentNode.parentNode.style.height = "0";
+            cardTitles[i].parentNode.parentNode.parentNode.style.marginBottom = "100px";
         }
+    }
+    catch
+    {
 
+    }
+
+    try {
         for (var i = 0; i < subNavList.length; i++) 
         {
             subNavList[i].style.backgroundColor = "#621d23";
@@ -66,8 +80,18 @@ window.onload = function()
 
         for (var i = 0; i < navItems.length; i++) 
         {
-            navItems[i].style.backgroundColor = "#9d4653";
+            navItems[i].style.backgroundColor = "#1f0506";
         }
+    }
+    catch 
+    {
+
+    }
+    
+    try {
+        document.getElementsByClassName('header-logo')[1].style.background = "url(https://i.pinimg.com/originals/89/6e/0a/896e0ad35109cb86273693350ccdd559.png)";
+        document.getElementsByClassName('header-logo')[1].childNodes[1].remove();
+        document.getElementsByClassName('page-aside-switch')[0].style.background = "#1f0506"
 
         document.getElementsByClassName('page-title')[0].style.color = "white";
         document.getElementsByClassName('page-title')[0].style.textAlign = "center";
@@ -79,7 +103,14 @@ window.onload = function()
         document.getElementsByClassName('breadcrumb')[0].remove(); 
         document.getElementsByClassName('toggle-submenu list-group-item list-group-item-action active')[0].style.backgroundColor = "#9b4551";
         document.getElementsByClassName('site-menubar-footer d-flex align-items-center')[0].childNodes[1].style.backgroundColor = "#9d4653";
-        document.getElementsByClassName('navbar-brand')[1].style.backgroundColor = "#2a090a";
+    }
+    catch
+    {
+        
+    }
+    
+    try {
+        document.getElementsByClassName('navbar-brand')[1].style.backgroundColor = "#1f0506";
         document.getElementsByClassName('navbar-options')[0].style.backgroundColor = "#1f0506";
         document.getElementsByClassName('fixed-top navbar')[1].style.boxShadow = "none";
         document.getElementsByClassName('list-group border-top')[0].remove();
